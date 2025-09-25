f8523a664091d467e8b94c4733df059e7de33940
Nick <nmf7508@g.rit.edu>
2025-09-25 10:03:13 -0400

    added main file, periods and prescalers need to be added for timer inits

--- proj/Lab05/main.c
+++ proj/Lab05/main.c
@@ -1,3 +1,18 @@
+#include "lab4/uart.h"
+#include "lab5/switches.h"
+#include "lab5/timers.h"
+#include "lab1/leds.h"
+
 int main() {
+	UART0_init();
+	LED1_init();
+	LED2_init();
+	S1_init_interrupt();
+	S2_init_interrupt();
+	TIMG6_init(0, 0);
+	TIMG12_init(0);
 	
+	while(1) {
+		;
+	}
 }
