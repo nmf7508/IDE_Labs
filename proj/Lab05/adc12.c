4e24f6794d53a5ce814b9e2cef32f293568e1498
Nick <nmf7508@g.rit.edu>
2025-09-30 10:35:18 -0400

    deleted PF from IOMUX (should be default fucntion) fixed ctl0 calls? chose memctl but no writes to it yet

--- proj/Lab05/adc12.c
+++ proj/Lab05/adc12.c
@@ -13,20 +13,21 @@ void ADC0_init(void) {
     }
 
     // Configure PA27 (ADC0.0) pin for analog
-    IOMUX->SECCFG.PINCM[IOMUX_PINCM60] = (IOMUX_PINCM_PC_CONNECTED | IOMUX_PINCM60_PF_ADC12_A0);
+    IOMUX->SECCFG.PINCM[IOMUX_PINCM60] = (IOMUX_PINCM_PC_CONNECTED);
 
     // Select clock (ULPCLK is fine, highest frequency range)
     ADC0->ULLMEM.GPRCM.CLKCFG = ADC12_CLKCFG_SAMPCLK_ULPCLK;
-    ADC0->ULLMEM.CTL0 = ADC12_CTL0_SCLKDIV_DIV_BY_1;
+    ADC0->ULLMEM.CTL0 = ADC12_CTL0_SCLKDIV_DIV_BY_8;
 
     // Configure for single channel, software trigger
     ADC0->ULLMEM.CTL0 = 0;
-    ADC0->ULLMEM.CTL0 |= ADC12_CTL0_PWREN_ENABLE;       // enable power
-    ADC0->ULLMEM.CTL0 |= ADC12_CTL0_CONVMODE_ONESHOT;   // one-shot conversion
-    ADC0->ULLMEM.CTL0 |= ADC12_EVT_MODE_INT0_CFG_SOFTWARE;       // software trigger
+    ADC0->ULLMEM.CTL0 |= ADC12_CTL0_PWRDN_MANUAL;       // enable power
+    ADC0->ULLMEM.CTL0 |= ADC12_CTL0_ENC_ON;					   // one-shot conversion
+		//doesnt look like you can set this reg
+    //ADC0->ULLMEM.EVT_MODE |= ADC12_EVT_MODE_INT0_CFG_SOFTWARE;       // software trigger
 
     // Map channel 0 (A0) to MEMRES0
-    ADC0->ULLMEM.MEMCTL = 0;   // use channel 0
+    ADC0->ULLMEM.MEMCTL[ADC12_MEMCTL_CHANSEL_CHAN_0] = 0;   // use channel 0
 }
 
 uint32_t ADC0_getVal(void) {
